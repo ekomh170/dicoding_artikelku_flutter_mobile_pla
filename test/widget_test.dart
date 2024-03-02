986e8236509ce6e76babddd968b0ecbf1305ccd2
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-03-03 00:06:21 +0700

    menyesuaikan file test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:dicoding_artikelku_flutter_mobile_pla/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
